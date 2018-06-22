209845adfaa38ec8c4fd04d01ed8214b18eab742
USER <USER@INET-18>
2018-06-22 19:52:13 +0900

    2018-06-22_Thread02(미완성)

---
+++ 2018-06-22_Thread02/src/FrGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class FrGUI {
+	static class MyGUI extends JFrame {
+		MyGUI() {
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setTitle("GUI");
+			setSize(500,500);
+			setVisible(true);
+		}
+	}
+}
+
+	
