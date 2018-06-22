ef877bcc7a87f5db2358769e6acbcd07ad057a5d
USER <USER@INET-18>
2018-06-22 11:46:56 +0900

    180622_Thread(미완성)

---
+++ 2018-06-22_Thread/src/FrServer.java
@@ -0,0 +1,39 @@
+import java.awt.BorderLayout;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class FrServer extends JFrame {
+	JTextArea ta;
+	JTextField tf;
+
+	public FrServer() {
+		setSize(600, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("채팅방 서버!");
+		setLayout(new BorderLayout());
+
+		JLabel label = new JLabel("This is a server!");
+		ta = new JTextArea(25, 40);
+		tf = new JTextField(25);
+
+		add(label, BorderLayout.NORTH);
+		add(ta, BorderLayout.CENTER);
+		add(tf, BorderLayout.SOUTH);
+		setVisible(true);
+		
+		
+	}
+
+}
+
